79a02a5d9408bcf1b29bcc730446fbfcc9bccc51
tom <didymusthomas6@gmail.com>
2026-03-26 04:52:10 +0300

    feat: add resources for authentication

---
+++ app/blueprints/auth/resources.py
@@ -0,0 +1,47 @@
+from flask_restful import Resource
+from flask import jsonify, request
+from .services import AuthService
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+
+class RegisterResource(Resource):
+    def post(self):
+        data = request.get_json()
+        result, status = AuthService.register(data)
+        return jsonify(result, status)
+
+
+class LoginResource(Resource):
+    def post(self):
+        data = request.get_json()
+        result, status = AuthService.login(data)
+        return jsonify(result, status)
+
+
+class LogoutResource(Resource):
+    def post(self):
+        data = AuthService.logout()
+        return jsonify(data)
+        
+
+class TokenRefreshResource(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        identity = get_jwt_identity()
+        data = AuthService.refresh(identity)
+        return jsonify(data)
+
+
+class CurrentUserResource(Resource):
+    @jwt_required()
+    def get(self):
+        identity = get_jwt_identity()
+        user = User.query.get(identity)
+
+        if not user:
+            return jsonify({
+                "error": "User not found"
+            }, 404)
+
+        data = AuthService.user_payload(user)
+        return jsonify(data, 200)
