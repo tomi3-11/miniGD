2843bcae42da7cf6cff7b3594c21d9cf824cb2ea
tom <didymusthomas6@gmail.com>
2026-03-26 05:08:05 +0300

    Update factory, auth blueprint registration and deps addition

--- app/__init__.py
+++ app/__init__.py
@@ -14,8 +14,11 @@ def create_app(config_object=Config):
 
     db.init_app(app)
 
-# Register blueprints
+    # Register blueprints
     from app.blueprints.home import home_bp
+    from app.blueprints.auth import auth_bp
+
     app.register_blueprint(home_bp)
+    app.register_blueprint(auth_bp)
 
     return app
