339c34719fd54b0cf79507155c3f8b13ac29533b
tom <didymusthomas6@gmail.com>
2026-03-28 00:03:25 +0300

    Update configs with mail configs

--- config.py
+++ config.py
@@ -10,3 +10,11 @@ class Config:
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=60)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=7)
 
+    # Mail configurations
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = os.getenv("MAIL_PORT ")
+    MAIL_USE_TLS = True 
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
+
