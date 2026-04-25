9a7bb19432f9a36d1e444857a129acec6d3798fc
tom <didymusthomas6@gmail.com>
2026-04-25 23:26:36 +0300

    Add file resources using the file service

---
+++ app/blueprints/files/resources.py
@@ -0,0 +1,69 @@
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.blueprint.files.service import FileService
+from flask import request, jsonify
+
+file_service = FileService()
+
+
+class FileListUploadResource(Resource):
+    
+    @jwt_required()
+    def post(self):
+        file = request.files.get("file")
+
+        if not file:
+            return jsonify({
+                "error": "No file provided"
+            }), 400
+
+        user_id = get_jwt_required()
+
+        saved_file = file_service.upload_file(file, user_id)
+
+        return jsonify({
+            "id": saved_file.id,
+            "filename": saved_file.filename
+        })
+
+
+    @jwt_required()
+    def get(self):
+        user_id = get_jwt_identity()
+
+        files = file_service.get_user_files(user_id)
+
+        return jsonify([
+            {
+                "id": f.id,
+                "filename": f.filename
+            } for f in files
+        ])
+
+
+
+class FileDownloadResource(Resource):
+    
+    @jwt_required()
+    def get(self, file_id):
+        user_id = get_jwt_identity()
+
+        url = file_service.get_download_url(file_id, user_id)
+
+        return jsonify({
+            "url": url
+        })
+
+
+class FileDeleteResource(Resource):
+    
+    @jwt_required()
+    def delete(self, file_id):
+        user_id = get_jwt_identity()
+
+        file_service.delete_file(file_id, user_id)
+
+        return jsonify({
+            "message": "Deleted"
+        })
+
